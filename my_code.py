16c27ea22f3d45ca096c9edc460bd8816e729945
kavyansh <wadhwakavyansh2008@gmail.com>
2026-01-28 21:06:11 +0530

    first commit

---
+++ my_code.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+data={
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [24, 30, 22, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+    }
+
+df = pd.DataFrame(data)
+
+data_dir='data'
+os.makedirs(data_dir, exist_ok=True)
+
+csv_path = os.path.join(data_dir, 'people.csv')
+
+df.to_csv(csv_path, index=False)
+
+print(f"DataFrame saved to {csv_path}")
